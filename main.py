73df1717e65c7f6fe362a0614f77a82dd590ef9e
Brandon Hinds <brandonhinds13@gmail.com>
2016-04-30 09:41:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from kivy.uix.popup import Popup
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.core.audio import SoundLoader
-from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
 
 from os import listdir, path
